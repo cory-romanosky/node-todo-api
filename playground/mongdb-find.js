1657f73767b550394559508e36ef6113e4f776e3
Cory Romanosky <cory.romanosky@gmail.com>
2018-02-22 21:12:11 -0800

    Initial Commit

---
+++ playground/mongdb-find.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable able to connect MongoDB server.');
+    }
+    console.log('Connected to MongoDB server.');
+
+    const db = client.db('TodoApp');
+
+    // db.collection('Todo').find({ 
+    //     _id: new ObjectID("5a8e4df50752e46303d16a5f")
+    // }).toArray().then((docs) => {
+    //     console.log('Todo');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch to do.', err)
+    // });
+
+    // db.collection('Todo').find().count().then((count) => {
+    //     console.log(`Todo count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch to do.', err)
+    // });
+
+    db.collection('Users').find({ name: "Cory Romanosky" }).toArray().then((docs) => {
+        console.log('Users');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch to do.', err)
+    });
+
+    // client.close();
+});
